0696b02d390c9871545f64f24383a03f53f2f9bf
Inge Wortel <ingewortel@gmail.com>
2020-07-23 14:12:53 +0200

    new analysis code phase diagram, parallel

---
+++ figures/scripts/analysis/fit-mixture-model-params.R
@@ -0,0 +1,45 @@
+library( celltrackR )
+library( mclust )
+library( dplyr )
+library( ggplot2 )
+
+argv <- commandArgs( trailingOnly = TRUE )
+
+
+dxfile <- argv[1]
+brokenfile <- argv[2]
+outfile <- argv[3]
+
+
+dxData <- readRDS( dxfile )
+broken <- readRDS( brokenfile )
+
+parmID <- names( dxData )[1]
+
+parms <-  unlist( strsplit( parmID, "-" ) )
+mact <- parms[1]
+lact <- parms[2]
+
+
+estimateComponents <- function( datalist ){
+  
+  fitsi <- lapply(datalist, function(x) Mclust( x, G=1:3,modelNames = "V" ))
+  names(fitsi) <- seq(1,length(fitsi))
+  
+  n <- sapply( fitsi, function(x) x$G )
+  BIC13 <- sapply(1:length(fitsi), function(x) as.numeric(fitsi[[x]]$BIC[3,1]) - as.numeric(fitsi[[x]]$BIC[1,1] ))
+  BIC12 <- sapply(1:length(fitsi), function(x) as.numeric(fitsi[[x]]$BIC[2,1]) - as.numeric(fitsi[[x]]$BIC[1,1] ))
+  BIC23 <- sapply(1:length(fitsi), function(x) as.numeric(fitsi[[x]]$BIC[3,1]) - as.numeric(fitsi[[x]]$BIC[2,1] ))
+  
+  df <- data.frame( n = n, dBIC13 = BIC13, dBIC12 = BIC12, dBIC23 = BIC23, mact = mact, lact = lact )
+  return(df)
+  
+}
+
+# fit mixture models to get num components and BICs
+fits <- lapply( dxData, estimateComponents )
+outdata <- bind_rows(fits)
+
+write.table( outdata, file = outfile, col.names=FALSE, row.names=FALSE, quote=FALSE)
+
+
---
+++ figures/scripts/analysis/get-dx-params.R
@@ -0,0 +1,52 @@
+library( celltrackR )
+library( ggplot2 )
+library( dplyr )
+
+argv <- commandArgs( trailingOnly = TRUE )
+
+
+trackfile <- argv[1]
+outfile <- argv[2]
+outplot <- argv[3]
+
+all.tracks <- readRDS( trackfile )
+
+getDx <- function(tracks ){
+  
+  nsim <- length(tracks)
+  
+  # steps from tracks, then shuffle them (wo regard for simulation they come from)
+  steps <- subtracks(tracks,1)
+  steps <- sample( steps, length( steps ) )
+  #steps <- steps[1:5000]
+  
+  # compute displacement and divide in nsim groups of displacements
+  disp <- t( sapply( steps, displacementVector) )[,1]
+  ind <- split(seq_along(disp), cut_number(seq_along(disp), nsim))
+  disp <- lapply( ind, function(x) disp[x] )
+  
+  return( disp )
+}
+
+# get the displacements
+dxData <- lapply( all.tracks, getDx )
+
+saveRDS( dxData, file = outfile )
+
+
+
+
+dxdfs <- lapply( 1:length(dxData[[1]] ) , function(x){ 
+	return( data.frame( num = x, 
+						dx = unname( dxData[[1]][[x]] ) ) )
+})
+dxdf <- bind_rows(dxdfs)
+
+
+p <- ggplot( dxdf, aes( x = dx ) ) + 
+	geom_density() + 
+	facet_wrap( ~num ) +
+	labs( title = names( all.tracks )[1] ) +
+	theme_bw()
+	
+ggsave( outplot, width = 7, height = 7 )
