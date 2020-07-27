3b6abfef4d466d76300ac0d512b66e1aac988139
Inge Wortel <ingewortel@gmail.com>
2020-07-27 14:10:49 +0200

    addition to analysis mixture models

--- figures/scripts/analysis/fit-mixture-model-params.R
+++ figures/scripts/analysis/fit-mixture-model-params.R
@@ -32,11 +32,23 @@ estimateComponents <- function( datalist ){
 	  names(fitsi) <- seq(1,length(fitsi))
   
 	  n <- sapply( fitsi, function(x) x$G )
+	  dmuRel <- sapply( fitsi, function(x) {
+	  	n <- x$G
+	  	if( n == 2){
+	  		dmu <- diff( x$parameters$mean )
+	  		sdmax <- sqrt( max( x$parameters$variance$sigmasq ) )
+	  		return( dmu/sdmax )
+	  	} else {
+	  		return( NA )
+	  	}
+	  })
+	  
+	  
 	  BIC13 <- sapply(1:length(fitsi), function(x) as.numeric(fitsi[[x]]$BIC[3,1]) - as.numeric(fitsi[[x]]$BIC[1,1] ))
 	  BIC12 <- sapply(1:length(fitsi), function(x) as.numeric(fitsi[[x]]$BIC[2,1]) - as.numeric(fitsi[[x]]$BIC[1,1] ))
 	  BIC23 <- sapply(1:length(fitsi), function(x) as.numeric(fitsi[[x]]$BIC[3,1]) - as.numeric(fitsi[[x]]$BIC[2,1] ))
   
-	  df <- data.frame( n = n, dBIC13 = BIC13, dBIC12 = BIC12, dBIC23 = BIC23, broken = broken2, mact = mact, lact = lact )
+	  df <- data.frame( n = n, dBIC13 = BIC13, dBIC12 = BIC12, dBIC23 = BIC23, broken = broken2, dmuRel = dmuRel, mact = mact, lact = lact )
   } else {
   	  df <- data.frame( n = NA, dBIC13 = NA, dBIC12 = NA, dBIC23 = NA, broken = broken2, mact = mact, lact = lact )
   }
